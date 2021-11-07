1b19a12764a370a58b7bfe208843c2e2d22e1c95
Adam Kjems <work@akjems.com>
2021-11-07 16:46:03 +0100

    add next meeting

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,18 @@ export default () => (
             <h1>Aalborg Toastmasters</h1>
             <p>Learn about <a href="https://www.toastmasters.org/">Toastmasters International</a></p>
 
+        <h2>Next meeting : 18/11-2021</h2>
+        <ul style="list-style-type:none">
+
+            <li>Location : <a href="https://goo.gl/maps/TYKZEF1ueKu8FaAw6">International House Rantzausgade 4, 9000 Aalborg</a></li>
+            <li>Online Access : <a href="https://tmclub.eu/portal.php?marknow=0">Tmclub.eu</a></li>
+            <li>Doors Open : 18:30</li>
+            <li>Meeting : 18:45 to 20:00</li>
+            <li>Chat with us on our <a href="https://www.facebook.com/ToastmastersAalborg">Facebook Page</a> or email info@aalborgtoastmasters.org</li>
+        </ul>
+
+        <img id="TMlogoWithDate" src="~/images/ColorLogoSince1924Navy_230px.jpg" alt="Toastmaster International Logo" />
+        <hr />
        
   </div>
 )

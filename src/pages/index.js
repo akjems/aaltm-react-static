ec9fe7e8cb29734ad58a37bbd0b90e3fa7e7745c
Adam Kjems <work@akjems.com>
2021-11-07 16:35:44 +0100

    tm content added

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,45 @@ import React from 'react'
 
 export default () => (
   <div style={{ textAlign: 'center' }}>
-    <h1>Welcome to Aalborg Toastmasters made with React-static</h1>
+
+            <h1 class="display-4">Aalborg Toastmasters</h1>
+            <p>Learn about <a href="https://www.toastmasters.org/">Toastmasters International</a></p>
+
+            <h2>Next meeting : 18/11-2021</h2>
+            <ul style="list-style-type:none">
+
+                <li>Location : <a href="https://goo.gl/maps/TYKZEF1ueKu8FaAw6">International House Rantzausgade 4, 9000 Aalborg</a></li>
+                <li>Online Access : <a href="https://tmclub.eu/portal.php?marknow=0">Tmclub.eu</a></li>
+                <li>Doors Open : 18:30</li>
+                <li>Meeting : 18:45 to 20:00</li>
+                <li>Chat with us on our <a href="https://www.facebook.com/ToastmastersAalborg">Facebook Page</a> or email info@aalborgtoastmasters.org</li>
+            </ul>
+
+            <img id="TMlogoWithDate" src="~/public/ColorLogoSince1924Navy_230px.jpg" alt="Toastmaster International Logo" />
+            <hr />
+
+            <h2>Past Meetings</h2>
+
+            <h4>Auroura : 04/11-2021</h4>
+            <ul style="list-style-type:none">
+                <li>Host : Anna</li>
+                <li>Location : <a href="https://goo.gl/maps/K2xqk5F9JxpDrgFD7">Aalborg Hovedbiblioteket Rendsburggade 2, 9000 Aalborg</a></li>
+                <li>Time : 18:45 to 20:30</li>
+                <li>Chat with us on our <a href="https://www.facebook.com/ToastmastersAalborg">Facebook Page</a> </li>
+            </ul>
+            <img id="meetingPhoto" src="~/public/Oct2_2021_500px.jpg" alt="Aalborg Toastmasters Meeting Photo" /><br /><br />
+            <img id="TMlogoWithDate" src="~/public/ColorLogoSince1924Navy_230px.jpg" alt="Toastmaster International Logo" />
+            <hr />
+
+            <h4>Bond, James Bond : 21/10-2021</h4>
+            <ul style="list-style-type:none">
+                <li>Host : Martina</li>
+                <li>Location : <a href="https://goo.gl/maps/K2xqk5F9JxpDrgFD7">Aalborg Hovedbiblioteket Rendsburggade 2, 9000 Aalborg</a></li>
+                <li>Time : 18:45 to 20:00</li>
+            </ul>
+            <img id="meetingPhoto" src="~/public/AalborgTM_Meeting_Oct_500px.jpg" alt="Aalborg Toastmasters Meeting Photo" /><br /><br />
+            <img id="TMlogoWithDate" src="~/public/ColorLogoSince1924Navy_230px.jpg" alt="Toastmaster International Logo" />
+
+       
   </div>
 )

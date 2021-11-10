cbed205ca9b6feb9c9d1573b623fe65b580347d9
Adam Kjems <work@akjems.com>
2021-11-10 20:53:26 +0100

    Add about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,6 +2,30 @@ import React from 'react'
 
 export default () => (
   <div>
-    <p>React Static is a progressive static site generator for React.</p>
+        <p>Aalborg Toastmasters core values are quality, creativity and consistency, and we take pride in providing opportunities for our members to grow and develop their skills.</p>
+
+            <h2> What can you expect when you come to Aalborg Toastmasters?</h2>
+           <p> We have members residing both in Denmark and Norway; therefore you can expect meetings to be hybrid - this means that some members attend online, and some members in person in Aalborg.
+
+            It is our finest task as Toastmasters to make you feel welcome, and make you feel included and encouraged to participate / practice public speaking in a safe and supportive environment.  As a guest, you need not participate - you may simply lean back and listen.
+                </p>
+            <h2>What takes place during a meeting?</h2>
+            <p>Toastmasters are trained in making meetings run smoothly. Bigger meeting roles are Toastmaster of the Day, Table Topics Master, General Evaluator and of course speakers (!), but you also have speech evaluators, timer, ah-counter and grammarian.
+            In general, meetings comprise three parts:</p>
+            <ol>
+                <li> Prepared Speeches (typically 5-7 min)</li>
+                <li> Impromptu Speeches aka Table Topics (1-2 min)</li>
+            <li> Constructive Feedback, called Evaluations in Toastmasters; (2-3 min)</li>
+            </ol>
+            You may be asked if you wish to answer a Table Topics question. It is of course okay if you decline. If you do agree to take a Table Topics question, and all of a sudden you find that it is hard to answer the question - you may tell the
+                Table Topics master that you will instead chose to answer an easier question such as "How was my day?"
+         
+            <h2>What happens if you decide to join?</h2>
+            <p>
+            The very first speech any Toastmaster does is called an "Icebreaker", it is an introduction of yourself, and it may take any format you like. It could be chronological, with focus on special life events. Or sharing of special interests or hobbies that are important to you.
+            In Aalborg Toastmasters we are serious about mentoring. If you decide to join the club, you will be allocated a mentor amongst our experienced members (based on your choice). A mentor can help guide you through the
+            onboarding process and get you used to Pathways and EasySPEAK. He or she can also help you with tips on how to handle nerves during your first speeches and how to find speech subjects which you find interesting and want to explore further.
+            </p><p>
+            We are looking forward to meeting you in Aalborg Toastmasters.</p>
   </div>
 )

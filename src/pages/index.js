cec33062e42dea16f0585cd4a53e4cb29554b5de
Adam Kjems <work@akjems.com>
2021-11-27 06:54:11 +0100

    update for next meeting

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import TM_logo from '../images/ColorLogoSince1924Navy_230px.jpg';
 import meetingImageOct from '../images/AalborgTM_Meeting_Oct_500px.jpg'
 import meetingImageOct2 from '../images/Oct2_2021_500px.jpg'
-
+import meetingImageNov from '../images/NovMeeting_500px.jpg'
 
 export default () => (
   <div style={{ textAlign: 'center' }}>
@@ -10,10 +10,10 @@ export default () => (
             <h1>Aalborg Toastmasters</h1>
             <p>Learn about <a href="https://www.toastmasters.org/">Toastmasters International</a></p>
 
-        <h2>Next meeting : 18/11-2021</h2>
+        <h2>Next meeting : 02/12-2021</h2>
         <ul>
 
-            <li>Location : <a href="https://goo.gl/maps/TYKZEF1ueKu8FaAw6">International House Rantzausgade 4, 9000 Aalborg</a></li>
+            <li>Location : <a href="https://goo.gl/maps/K2xqk5F9JxpDrgFD7">Aalborg Hovedbiblioteket Rendsburggade 2, 9000 Aalborg</a></li>
             <li>Online Access : <a href="https://tmclub.eu/portal.php?marknow=0">Tmclub.eu</a></li>
             <li>Doors Open : 18:30</li>
             <li>Meeting : 18:45 to 20:00</li>
@@ -23,6 +23,18 @@ export default () => (
       
         <img src={ TM_logo } alt="Toastmaster International Logo" />
         <hr />
+
+        <h4>TM Hybrid Meeting : 18/11-2021</h4>
+        <ul>
+
+            <li>Location : <a href="https://goo.gl/maps/TYKZEF1ueKu8FaAw6">International House Rantzausgade 4, 9000 Aalborg</a></li>
+            <li>Time : 18:45 to 20:30</li>
+        </ul>
+        <img id="meetingPhoto" src={meetingImageNov} alt="Aalborg Toastmasters Meeting Photo" /><br /><br />
+        <img id="TMlogoWithDate" src={TM_logo} alt="Toastmaster International Logo" />
+        <hr />
+
+
         <h4>Auroura : 04/11-2021</h4>
         <ul>
             <li>Host : Anna</li>
